fa5dc8f790935f0b456ea0717aeb6b88585705e9
hashan.silva <hashan.silva@gmail.com>
2022-06-11 14:50:54 +0530

    HR:C:A very big sum challenge

---
+++ src/com/hackerrank/challenges/AVeryBigNumber.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022. Hashan Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.hackerrank.challenges;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class AVeryBigNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    private static class Result {
+        public static long aVeryBigSum(List<Long> ar) {
+            return ar.stream().mapToLong(Long::longValue).sum();
+        }
+    }
+}
