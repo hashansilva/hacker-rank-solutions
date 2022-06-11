10d71d521ba4b9848524e2713d7f6a775808c883
hashan.silva <hashan.silva@gmail.com>
2022-06-11 06:15:18 +0530

    HR:C:compare triplet

--- src/com/hackerrank/challenges/CompareTriplets.java
+++ src/com/hackerrank/challenges/CompareTriplets.java
@@ -14,5 +14,57 @@
 
 package com.hackerrank.challenges;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class CompareTriplets {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    private static class Result {
+        public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+            List<Integer> result = new ArrayList<>();
+            int aliceResult = 0;
+            int bobResult = 0;
+            for (int i = 0; i < 3; i++) {
+                int intA = a.get(i);
+                int intB = b.get(i);
+                if (intA > intB) {
+                    aliceResult = aliceResult + 1;
+                } else if (intA < intB) {
+                    bobResult = bobResult + 1;
+                }
+            }
+            result.add(aliceResult);
+            result.add(bobResult);
+            return result;
+        }
+    }
 }
