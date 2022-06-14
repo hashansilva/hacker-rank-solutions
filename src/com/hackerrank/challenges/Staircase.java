01680cf2802063b9bd48924d332ff812c01fc37d
hashan.silva <hashan.silva@gmail.com>
2022-06-14 14:33:34 +0530

    HR:C:staircase challenge

---
+++ src/com/hackerrank/challenges/Staircase.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022. Hashan Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.hackerrank.challenges;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Staircase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+
+    private static class Result {
+        public static void staircase(int n) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = n; j > 0; j--) {
+                    if (j > i) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("#");
+                    }
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+}
