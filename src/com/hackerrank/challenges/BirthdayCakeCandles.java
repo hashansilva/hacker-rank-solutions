4e504f30a273bb1fd91b20a688b98c265d253723
hashan.silva <hashan.silva@gmail.com>
2022-06-16 23:06:09 +0530

    HR:C:birthday cake candles challenge

---
+++ src/com/hackerrank/challenges/BirthdayCakeCandles.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022. Hashan Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.hackerrank.challenges;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class BirthdayCakeCandles {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    private static class Result {
+        public static int birthdayCakeCandles(List<Integer> candles) {
+            int result;
+            Collections.sort(candles);
+            Collections.reverse(candles);
+            int tallestCandle = candles.get(0);
+            result = (int) candles.stream().filter(candle -> candle == tallestCandle).count();
+            return result;
+        }
+    }
+}
