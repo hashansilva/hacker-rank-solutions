02a45fbd663e1fec76b6aa0e843bfe861c7ca0f4
hashan.silva <hashan.silva@gmail.com>
2022-06-20 09:21:35 +0530

    HR:C:Grading Students challenge

---
+++ src/com/hackerrank/challenges/implementation/GradingStudents.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2022. Hashan Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.hackerrank.challenges.implementation;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class GradingStudents {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    private static class Result {
+        public static List<Integer> gradingStudents(List<Integer> grades) {
+            List<Integer> results = new ArrayList<>();
+            for (Integer grade : grades) {
+                int roundFactor = 0;
+                int resultGrade = grade;
+                int multiplyIndex = grade % 5;
+                if (multiplyIndex > 0) {
+                    roundFactor = 5 - multiplyIndex;
+                }
+                if (grade >= 38 && roundFactor < 3) {
+                    resultGrade = grade + roundFactor;
+                }
+                results.add(resultGrade);
+
+            }
+
+            return results;
+        }
+    }
+}
