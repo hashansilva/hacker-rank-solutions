8d6ea2dbf169ad4e59a8adf2a377c84221fc97e2
hashan.silva <hashan.silva@gmail.com>
2022-06-29 10:20:19 +0530

    HR:C:number line jump challenge

---
+++ src/com/hackerrank/challenges/implementation/NumberLineJump.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022. Hashan Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.hackerrank.challenges.implementation;
+
+import java.io.*;
+
+public class NumberLineJump {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = Result.kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    private static class Result {
+        public static String kangaroo(int x1, int v1, int x2, int v2) {
+            int vdiff = v1 - v2;
+            int xdiff = x1 - x2;
+            if (xdiff <=0 && vdiff <= 0) {
+                return "NO";
+            }
+            return (xdiff % vdiff == 0 ? "YES" : "NO");
+        }
+    }
+}
