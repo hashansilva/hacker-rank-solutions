9d499b47fe9122b24a9eca21d01a5146f2b7b73e
hashan.silva <hashan.silva@gmail.com>
2022-08-05 23:12:08 +0530

    HR:C:adding solutions for BillDivision

---
+++ src/com/hackerrank/challenges/implementation/BillDivision.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022. Hashan Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.hackerrank.challenges.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class BillDivision {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+
+    private static class Result {
+        public static void bonAppetit(List<Integer> bill, int k, int b) {
+            int billActual = 0;
+            for (int index = 0; index < bill.size(); index++) {
+                if (index != k) {
+                    billActual = billActual + bill.get(index);
+                }
+            }
+            billActual = billActual / 2;
+            if (b - billActual == 0) {
+                System.out.println("Bon Appetit");
+            }else{
+                System.out.println((b - billActual));
+            }
+        }
+    }
+}
